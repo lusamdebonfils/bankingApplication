6964ecf613473ba7f5270cb881e7d2fdab7a70ae
lusamdebonfils <samuelluswata@gmail.com>
2019-08-06 19:18:15 -0500

    Initial commit

---
+++ src/main/java/edu/mum/cs/finalprep/bankingapplication/BankingapplicationApplication.java
@@ -0,0 +1,13 @@
+package edu.mum.cs.finalprep.bankingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingapplicationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/mum/cs/finalprep/bankingapplication/BankingapplicationApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.mum.cs.finalprep.bankingapplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BankingapplicationApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
